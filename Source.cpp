cdae544b180e601170f2cf6b5f70fc93c92537db
bsonge <w909781@usm.edu>
2016-11-29 19:53:40 -0600

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -11,6 +11,12 @@ using namespace std;
 string infile = "addresses.txt";
 vector<string> addresses;
 
+//global structs
+struct splitAddress
+{
+	string page = "", offset = "";
+};
+
 //functions
 vector<string> getAddresses(vector<string>& addresses, string filename);
 
@@ -18,6 +24,15 @@ vector<string> getAddresses(vector<string>& addresses, string filename);
 // -----------------[MAIN]----------------- //
 int main() {
 	getAddresses(addresses, infile);
+	vector<splitAddress> splitAddresses;
+	
+	for (vector<string>::iterator it = addresses.begin(); it < addresses.end(); it++) {
+		splitAddress temp;
+		temp.page = it->substr(0, 8); //0 to 7 does not store 7 for some reason so using 0 to 8
+		temp.offset = it->substr(8, 15);
+		splitAddresses.push_back(temp);
+		cout << *it << ", "  << temp.page << ", " << temp.offset << endl;
+	}
 
 	system("pause");
 	return 0;
@@ -41,6 +56,7 @@ vector<string> getAddresses(vector<string>& addresses, string filename)
 		addresses.push_back(temp);
 	}	
 	
+	
 	//int i = 0; //counter
 	for (vector<string>::iterator it = addresses.begin(); it < addresses.end(); it++) //convert to 16 bit binary
 	{
@@ -48,17 +64,11 @@ vector<string> getAddresses(vector<string>& addresses, string filename)
 		stringstream convert(*it);
 		convert >> temp;
 		*it = bitset<16>(temp).to_string();
-
+		
+		//cout << *it << endl;
 	}
 	
-	
-	//mask
-	/*
-
-		fun masking code goes here
-	
-	*/
-
+	addresses.pop_back(); //for some reason it keeps storing an extra element 0: this gets rid of it
 
 	ifile.close();
 	return addresses;
